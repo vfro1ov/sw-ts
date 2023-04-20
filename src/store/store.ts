ae14580f14e281200e3225b377bbfa41bcb9542f
Vladimir F <vof9696@gmail.com>
2023-04-20 23:28:08 +0300

    add ListenerMiddleware

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,22 @@
-import {configureStore } from '@reduxjs/toolkit';
-import favoritesReducer from './features/favorites';
+import {configureStore, createListenerMiddleware, isAnyOf } from '@reduxjs/toolkit';
+import favoritesReducer, { setFavorites, removeFavorites} from './features/favorites';
 // import rootReduser from './redusers'
+
+const listenerMiddleware = createListenerMiddleware()
+listenerMiddleware.startListening({
+	matcher: isAnyOf(setFavorites, removeFavorites),
+    effect: () => {
+			return (
+				localStorage.setItem('favorites', JSON.stringify(store.getState().favorites))
+			);
+		}
+})
+
 const store = configureStore({
 	reducer:{
 		favorites:favoritesReducer
 	},
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(listenerMiddleware.middleware)
 })
 
 export default store;
