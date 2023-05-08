04074ad574d9ebd801a88eac1e052532efd6c3bd
Vladimir F <vof9696@gmail.com>
2023-05-08 12:53:28 +0300

    styled HomePage

--- src/components/HomePage/PageNav/PageNav.tsx
+++ src/components/HomePage/PageNav/PageNav.tsx
@@ -11,13 +11,21 @@ const PageNav: FunctionComponent<PageNavProps> = (props) => {
 		{ name: 'films', link: '/films', bgimg: 'https://eksmo.ru/upload/iblock/8af/Exmo_0005_full_min.jpg' },
 		{ name: 'favorites', link: '/favorites', bgimg: 'https://eksmo.ru/upload/iblock/8af/Exmo_0005_full_min.jpg' },
 	];
-	const bgImage = `${pages[2]}`
+	const bgimage = ['https://img2.akspic.ru/crops/9/3/6/1/7/171639/171639-zvezdnye_vojny_epizod_3_ps2-playstation_2-mest_zvezdnyh_vojn-dart_vejder-zvezdnye_vojny_vojny_klonov-720x1280.jpg',
+	'https://img1.akspic.ru/crops/5/0/5/1/41505/41505-zvezdnye_vojny-enakin_skajuoker-droid-bronya-rycar-720x1280.jpg',
+	'https://img1.akspic.ru/crops/0/4/6/1/7/171640/171640-poster_sagi_o_skajuokerah_zvezdnyh_vojn-dzhordzh_lukas-lego_zvezdnye_vojny_saga_o_skajuokerah-zvezdnye_vojny_vosstanie_skajuoker-mark_hemill-720x1280.jpg',
+	'https://img1.akspic.ru/crops/3/0/8/3/6/163803/163803-art-asoka_tano-dart_vejder-zvezdnye_vojny_vojny_klonov-poster-720x1280.jpg'
+]
+	const bgImage = {
+		backgroundImage: `url('${bgimage[1]}')`
+	}
+	console.log(bgImage)
 	return (
 		<>
 			<div className="pages">
 				{pages.map((page,index) => (
 					<Link to={page.link}>
-					<div className="pages_element" style={{ backgroundImage:bgImage}} key={index}>
+					<div className="pages_element"  key={index} style={{backgroundImage: `url('${bgimage[index]}')`}}>
 						<p>{page.name}</p>
 					</div>
 					</Link>
